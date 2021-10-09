311d26aa226ca1437674161c99442b4b6eaac5e1
xuefeng <2106919811@qq.com>
2021-10-09 17:54:21 +0800

    Refactoring run_game method

--- alien_invasion.py
+++ alien_invasion.py
@@ -20,21 +20,24 @@ class AlienInvasion:
 
         self.ship = Ship(self)
 
-
     def run_game(self):
         """开始游戏的主循环"""
         while True:
-            # 监视键盘和鼠标事件。
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
+            self._update_screem()
 
-            # 每次循环时都重绘屏幕。
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
+    def _check_events(self):
+        """响应按键和鼠标事件。"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
 
-            # 让最近绘制的屏幕可见。
-            pygame.display.flip()
+    def _update_screem(self):
+        """更新屏幕上的图像，并切换到新屏幕。"""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        pygame.display.flip()
 
 
 if __name__ == '__main__':
